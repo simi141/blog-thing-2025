75b77ebe610fc1bcf83142e533b1d7a65857dfa1
Simran <sbhatti3811@gmail.com>
2025-05-31 14:49:14 -0700

    basic javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+let name = "Simran";
+let age = 14;
+let likesSushi = false;
+
+function greeting(name) {
+    console.log("Hello", name);
+}
+
+greeting("Simran");
+
+let newName = document. getElementById("greeter").ariaValueMax;
